a30f7b390926a28068809bbde99abfab2727b853
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-16 16:41:14 -0300

    refactor task rendering and update task completion logic

--- js/index.js
+++ js/index.js
@@ -4,23 +4,33 @@ const taskTitleForm = document.querySelector("#task-title-input");
 const toDoListUl = document.querySelector("#to-do-list");
 
 let tasks = [];
-
-form.addEventListener("submit", (evento) => {
-  evento.preventDefault(); // Evita comportamento de recarregar a pagina quando submeter
-
-  const taskTitle = taskTitleForm.value;
-
-  if (taskTitle.length < 3) {
-    alert("Sua tarefa prescisa ter pelo menos 3 caracteres");
-    return;
-  }
-  tasks.push({
-    title: taskTitle,
-    done: false,
-  });
-
-  const input = document.createElement("input"); // <input
+function renderTasksOnHtml(taskTitle, done ='false'){
+    const input = document.createElement("input"); // <input
   input.setAttribute("type", "checkbox"); // <input type="checkbox">
+
+  input.addEventListener('change', (event) =>{
+    const liToToggle = event.target.parentElement;
+
+    const done = event.target.checked
+
+    const spanToToggle = liToToggle.querySelector('span')
+    if(done){
+        spanToToggle.style.textDecoration = 'line-through'
+    }else{
+        spanToToggle.style.textDecoration = 'none'
+    }
+    tasks = tasks.map(tarefa => {
+        if(tarefa.title === spanToToggle.textContent){
+            return{
+                title:tarefa.title,
+                done:!tarefa.done
+            }
+        }
+
+        return tarefa
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  })
 
   const span = document.createElement("span");
   span.textContent = taskTitle;
@@ -31,8 +41,10 @@ form.addEventListener("submit", (evento) => {
   button.addEventListener("click", (event) => {
     const liToRemove = event.target.parentElement;
     const titleToRemove = liToRemove.querySelector("span");
-    tasks.filter((tarefa) => tarefa.title !== titleToRemove);
+    tasks = tasks.filter((tarefa) => tarefa.title !== titleToRemove.textContent);
     toDoListUl.removeChild(liToRemove);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
   });
 
   //Adcionando nova tarefa
@@ -44,4 +56,41 @@ form.addEventListener("submit", (evento) => {
   toDoListUl.appendChild(li);
 
   taskTitleForm.value = "";
+
+ input.checked = done
+
+ span.textContent = taskTitle
+
+ if(done){
+     spanToToggle.style.textDecoration = 'line-through'
+ }
+}
+window.onload = () => {
+    const taskLocalStorage = localStorage.getItem('tasks')
+
+    if(!taskLocalStorage) return
+
+    tasks = JSON.parse(taskLocalStorage)
+
+    tasks.forEach(tarefa => {
+        renderTasksOnHtml(tarefa.title, tarefa.done)
+    });
+  }
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault(); // Evita comportamento de recarregar a pagina quando submeter
+
+
+  const taskTitle = taskTitleForm.value;
+
+  if (taskTitle.length < 3) {
+    alert("Sua tarefa prescisa ter pelo menos 3 caracteres");
+    return;
+  }
+  tasks.push({
+    title: taskTitle,
+    done: false,
+    
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+  
 });
