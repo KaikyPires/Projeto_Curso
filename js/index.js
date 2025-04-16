21cf0b2e3235e084805a23fc19376d3214bcc071
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-04-16 15:41:51 -0300

    refactor form submission logic and enhance task removal functionality

--- js/index.js
+++ js/index.js
@@ -1,24 +1,47 @@
-const form = document.querySelector('#to-do-form')
-const taskTitleForm = document.querySelector('#task-title-input')
+const form = document.querySelector("#to-do-form");
+const taskTitleForm = document.querySelector("#task-title-input");
 
-const toDoListUl = document.querySelector('#to-do-list')
+const toDoListUl = document.querySelector("#to-do-list");
 
-let tasks = []
+let tasks = [];
 
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault(); // Evita comportamento de recarregar a pagina quando submeter
 
-form.addEventListener('submit', (evento) =>{
-    evento.preventDefault()// Evita comportamento de recarregar a pagina quando submeter
-    
-    const taskTitle = taskTitleForm.value
+  const taskTitle = taskTitleForm.value;
 
-   if(taskTitle.length < 3){
-    alert('Sua tarefa prescisa ter pelo menos 3 caracteres')
-    return
-   }
-   tasks.push(taskTitle)
+  if (taskTitle.length < 3) {
+    alert("Sua tarefa prescisa ter pelo menos 3 caracteres");
+    return;
+  }
+  tasks.push({
+    title: taskTitle,
+    done: false,
+  });
 
-   //Adcionando nova tarefa
-    const li = document.createElement('li')
-    li.textContent = taskTitle
-   toDoListUl.appendChild(li)
-})
+  const input = document.createElement("input"); // <input
+  input.setAttribute("type", "checkbox"); // <input type="checkbox">
+
+  const span = document.createElement("span");
+  span.textContent = taskTitle;
+
+  const button = document.createElement("button");
+  button.textContent = "Remover";
+
+  button.addEventListener("click", (event) => {
+    const liToRemove = event.target.parentElement;
+    const titleToRemove = liToRemove.querySelector("span");
+    tasks.filter((tarefa) => tarefa.title !== titleToRemove);
+    toDoListUl.removeChild(liToRemove);
+  });
+
+  //Adcionando nova tarefa
+  const li = document.createElement("li");
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  toDoListUl.appendChild(li);
+
+  taskTitleForm.value = "";
+});
